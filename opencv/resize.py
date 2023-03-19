16c646431cbbc89bec92541df340e1b1eed359df
kch2056 <goodchance2056@gmail.com>
2023-03-19 12:29:16 +0900

    :memo:update

--- opencv/resize.py
+++ opencv/resize.py
@@ -1,15 +1,16 @@
 import cv2
 
-file_path = "opencv\Lenna.png"
+file_path = input()
+file_path = "opencv\\" + file_path
 
 img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
 
-print(img)
+width = int(input('width? '))
+height = int(input('height? '))
+resized_img = cv2.resize(img, (width, height))
 
-resized_img = cv2.resize(img, (600, 600))
 
-
-cv2.imshow('Lenna', img)
+cv2.imshow('file', img)
 cv2.imshow('resized', resized_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
